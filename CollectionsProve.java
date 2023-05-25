08f2efda64349f961617ec040870b64b0bb121cd
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-25 12:05:31 +0200

    version 1 main

---
+++ CollectionsProve.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionsProve
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+        list.add("Grapes");
+        list.add("Strawberry");
+
+        //Collections.sort(list);
+
+        Collections.replaceAll(list, "Banana", "Gatto") ;
+        //Collections.shuffle(list);
+        Collections.swap(list,0, 3);;
+
+        System.out.println("Sorted list: " +list);
+        for (String  fruit : list)
+        {
+            System.out.println(fruit);
+        }
+    }
+    
+}
